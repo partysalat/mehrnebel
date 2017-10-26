3760d77c7820da40db6de719f45a2174a5c49231
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-10-26 18:30:14 +0200

    Introduces hammerjs

--- lib/browser/app/buzzer/arcadeButton/ArcadeButton.jsx
+++ lib/browser/app/buzzer/arcadeButton/ArcadeButton.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Hammer from 'react-hammerjs';
 import './ArcadeButton.styl';
 
-export default ({ onClick }) => <button onClick={onClick} className="push--flat" />;
 
+export default class ArcadeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressed: false,
+    };
+  }
+  handleOnPressUp= (args) => {
+    console.log('ON PRESS UP', args);
+  }
+  handleOnTap= (args) => {
+    console.log('ON TAP', args);
+  }
+  handleOnPress= (args) => {
+    console.log('ON PRESS', args);
+  }
+  render() {
+    const className = this.state.pressed ? 'is-pushed' : '';
+    return (
+      <Hammer
+        onPress={this.handleOnPress}
+        onPressUp={this.handleOnPressUp}
+        onTap={this.handleOnTap}
+      >
+        <button className={`push--flat ${className}`} />
+      </Hammer>
+    );
+  }
+}
