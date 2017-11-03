58ea811bdd00d7909475824ee0193cdf4cc12367
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-11-03 15:57:36 +0100

    * Update Stats for user accordingly

--- lib/browser/app/buzzer/Buzzer.jsx
+++ lib/browser/app/buzzer/Buzzer.jsx
@@ -6,20 +6,33 @@ import getInstance from './../login/httpService';
 export default class Buzzer extends Component {
   static async createFog() {
     const client = await getInstance();
-    return client.put('/api/create-fog').then((res) => {
-      console.log(res.data);
-    });
+    const res = await client.put('/api/create-fog');
+    return res;
   }
 
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {};
+    this.initUser();
+  }
+  async initUser() {
+    const client = await getInstance();
+    const user = await client.get('/api/user');
+    console.log(user);
+    this.setState(currentState => ({ ...currentState, user: user.data.data }));
+  }
+  createFogAndUpdateState = () => {
+    Buzzer.createFog()
+      .then((user) => {
+        this.setState(currentState => ({ ...currentState, user: user.data.data }));
+      });
   }
 
   render() {
     return (
       <div>
-        <CreateFogButton createFog={Buzzer.createFog} />
+        <div>{JSON.stringify(this.state.user)}</div>
+        <CreateFogButton createFog={this.createFogAndUpdateState} />
       </div>
     );
   }
