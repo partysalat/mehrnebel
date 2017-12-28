92602e2baea3abeb44a47ee17cf820d5ec042b99
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-12-28 22:20:01 +0100

    Add on click for logout

--- lib/browser/app/buzzer/LogoutButton/LogoutButton.jsx
+++ lib/browser/app/buzzer/LogoutButton/LogoutButton.jsx
@@ -1,9 +1,9 @@
-
 import * as React from 'react';
 import './LogoutButton.styl';
+import { logout } from '../../login/authService';
 
 export default function () {
   return (
-    <div className="fa fa-power-off logout-button" aria-hidden="true" />
+    <div className="fa fa-power-off logout-button" aria-hidden="true" onClick={() => logout()} />
   );
 }
