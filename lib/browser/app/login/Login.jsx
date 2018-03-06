d2368ac7742fd4f282c089e43a0e81068cf43f52
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-03-06 18:43:15 +0100

    replace state when accessing /buzzer route

--- lib/browser/app/login/Login.jsx
+++ lib/browser/app/login/Login.jsx
@@ -4,11 +4,6 @@ import './Login.styl';
 
 
 export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
-
   handleLogin = () => {
     this.props.history.push('/buzzer');
   }
