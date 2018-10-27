66e89ab678d4dc33b156807af7b467d17f422d90
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-10-27 17:52:41 +0200

    Add fix for animation for grayscale filter (iphone) and change positioning and color of claim button

--- lib/browser/app/buzzer/ClaimMutexButton/ClaimMutexButton.jsx
+++ lib/browser/app/buzzer/ClaimMutexButton/ClaimMutexButton.jsx
@@ -5,7 +5,7 @@ import './ClaimMutexButton.styl';
 
 export default function ClaimMutexButton(props) {
   return (
-    <div>
+    <div className="claim-mutex-button">
       <label className="switch">
         <input type="checkbox" onClick={props.onClick} className={props.isChecked && 'checked'} />
         <span className="slider round" />
