b443d7a45226789bbc60707104c1aee569b51a04
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-10-27 17:38:43 +0200

    Add visibility state to disable token claim when user turns off phone

--- lib/browser/app/buzzer/CreateFogButton/CreateFogButton.jsx
+++ lib/browser/app/buzzer/CreateFogButton/CreateFogButton.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Hammer from 'react-hammerjs';
 import './CreateFogButton.styl';
 
-function CreateFogButton(props) {
-  return (
-    <Hammer
-      onPress={props.createFog}
-      onPressUp={props.createFogEnd}
-      onPanEnd={props.createFogEnd}
-      onTap={props.createFogSimple}
-    >
-      <button className={['push--flat', !props.enabled && 'disabled'].join(' ')} />
-    </Hammer>
-  );
+class CreateFogButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPressed: false,
+    };
+    this.createFog = this.createFog.bind(this);
+    this.createFogEnd = this.createFogEnd.bind(this);
+    this.createFogSimple = this.createFogSimple.bind(this);
+  }
+  setPressed(value) {
+    this.setState({ isPressed: value });
+  }
+  createFog() {
+    this.setPressed(true);
+    this.props.createFog();
+  }
+  createFogEnd() {
+    this.setPressed(false);
+    this.props.createFogEnd();
+  }
+  createFogSimple() {
+    this.setPressed(true);
+    this.props.createFogSimple();
+    setTimeout(() => this.setPressed(false), 200);
+  }
+  render() {
+    return (
+      <Hammer
+        onPress={this.createFog}
+        onPressUp={this.createFogEnd}
+        onPanEnd={this.createFogEnd}
+        onTap={this.createFogSimple}
+
+      >
+        <button className={[
+          'push--flat',
+          !this.props.enabled && 'disabled',
+          this.state.isPressed && 'is-pushed',
+        ].join(' ')}
+        />
+      </Hammer>
+    );
+  }
 }
 
 CreateFogButton.propTypes = {
