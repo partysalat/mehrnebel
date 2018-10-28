f6d97747efb02bec3fd7ec5903df9fd0c1faeda8
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-10-28 19:42:05 +0100

    Replace react with preact

--- webpack/webpack.browser.base.config.js
+++ webpack/webpack.browser.base.config.js
@@ -11,6 +11,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
   },
   externals: {
     ws: 'window.WebSocket',
